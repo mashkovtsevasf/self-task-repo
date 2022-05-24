0258e88273f2d69838be284385bf4bd9f778a0aa
Sofia Mashkovtseva <mashkovtseva.ssff@gmail.com>
2022-05-25 02:14:34 +0300

    added new files

---
+++ index.js
@@ -0,0 +1,10 @@
+const question01 = prompt('Number');
+const question02 = prompt('Number');
+const number01 = Number(question01);
+const number02 = Number(question02);
+alert(number01 + number02)
+
+// alternative variant:
+// const question01 = prompt('Number');
+// const question02 = prompt('Number');
+// alert(Number(question01) + Number(question02))
